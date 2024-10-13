b8f903e3e7b0614d7b675723b3b47ae0a5e166f7
Sush1090 <sushrut1090@gmail.com>
2024-10-13 18:41:33 +0200

    Update PhasePlot.jl

--- src/PhasePlot.jl
+++ src/PhasePlot.jl
@@ -1,7 +1,12 @@
 
 
+abstract type PhasePlotType end
 
-function PhasePlot()
+struct PhasePlotType_PH <: PhasePlotType end
+struct PhasePlotType_TS <: PhasePlotType end
+
+
+function PhasePlot(type::PhasePlotType = PhasePlotType_TS(),sol::ODESolution)
     
 end
 
