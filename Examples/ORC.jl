cc1c65c7e738abc6704a4fb45905dcd0dd717c42
Sush1090 <sushrut1090@gmail.com>
2024-10-15 15:05:36 +0200

    Plots ORC-TS

--- Examples/ORC.jl
+++ Examples/ORC.jl
@@ -5,7 +5,7 @@ using CoolPropCycles, ModelingToolkit, DifferentialEquations, CoolProp
 fluid = "R134A"
 _system = Isentropic_η(η = 1,πc = 5) # fix the isentropic Efficiency of compressor and pressre ratio
 
-start_T = 300; # Temperature at source 
+start_T =  292.89105203453886; # Temperature at source 
 start_p = PropsSI("P","Q",0,"T",start_T,fluid) + 1e3 # pressure at source.
 # As it is ORC the inlet state is liquid and bit away from saturation curv. Hence 1e3Pa of pressure is added
 ΔT_subcool = PropsSI("T","P",start_p,"Q",0,fluid) - start_T; # ensure the subcoolin temperature to reach bck to starting state.
@@ -15,7 +15,7 @@ start_h = PropsSI("H","T",start_T,"P",start_p,fluid); start_mdot = 0.2 #kg/s
 
 @named source = MassSource(source_enthalpy = start_h,source_pressure = start_p,source_mdot = start_mdot,fluid = fluid)
 @named comp = Compressor(_system, fluid =fluid)
-@named evap = SimpleEvaporator(Δp = [0,0,0],ΔT_sh = 5,fluid = fluid)
+@named evap = SimpleEvaporator(Δp = [0,0,0],ΔT_sh = 23.054838257544585,fluid = fluid)
 @named exp = Expander(_system,fluid= fluid)
 @named cond = SimpleCondensor(ΔT_sc = ΔT_subcool,Δp = [0,0,0],fluid = fluid)
 @named sink = MassSink(fluid = fluid)
@@ -38,9 +38,12 @@ sys = structural_simplify(dis_test)
 prob = ODEProblem(sys,u0,tspan,guesses = [])
 sol = solve(prob)
 
+
+
 #compute Efficiency of the cycle.
 #Note: sign convetion: Power supplied to the system is +ve while from thee system is -ve
 @show η = (sol[exp.P][1] + sol[comp.P][1])/sol[evap.P][1]
 
 #Check if the final state is close to the inital state. 
 Compute_cycle_error(sol,systems)
+CoolPropCycles.PhasePlot(PhasePlotType_TS(),sol,systems,fluid)
