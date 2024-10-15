4bc21aba21da8ceec702f28a05bd078c70f87c4b
Sush1090 <sushrut1090@gmail.com>
2024-10-15 11:30:01 +0200

    added cycle matching error

--- src/ThemodynamicClaculations.jl
+++ src/ThemodynamicClaculations.jl
@@ -1,4 +1,31 @@
 
+"""
+`Compute_cycle_error(p_source,h_source,p_sink,h_sink;reltol = 1e-8)`
+    * Computes cycle end point mismatch in states - between sink and source
+"""
+function Compute_cycle_error(p_source,h_source,p_sink,h_sink;reltol = 1e-8)
+    err_p = abs(p_source - p_sink)/p_source
+    err_h = abs(h_source - h_sink)/h_source
+    if err_p > reltol && err_h <reltol
+        @warn "Pressure mismatch between cycle source and sink"
+    end
+    if err_h > reltol && err_p < reltol
+        @warn "Enthalpy mismatch between cycle source and sink"
+    end
+    if err_h > reltol && err_p > reltol
+        @warn "Enthalpy and pressure mismatch between cycle source and sink"
+    end
+    return nothing
+end
+
+function Compute_cycle_error(sol::ODESolution,system::Vector{ODESystem};reltol = 1e-8)
+    source = system[1]; sink = system[end]
+    h_source = sol[source.h][1]; p_source = sol[sink.p][1];
+    h_sink = sol[sink.h][1]; p_sink = sol[sink.p][1];
+    Compute_cycle_error(p_source,h_source,p_sink,h_sink,reltol=reltol)
+end
+
+export Compute_cycle_error
 
 function PitchPoint()
     
