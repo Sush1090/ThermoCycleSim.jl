93112af7e2336986e9451248ad53b8c8380f0454
Sush1090 <sushrut1090@gmail.com>
2024-10-15 22:46:22 +0200

    removed comments

--- src/Processes.jl
+++ src/Processes.jl
@@ -1,22 +1,6 @@
 
 using CoolProp, ModelingToolkit
 
-# """
-# `IsentropicCompression(πc, h_in, p_in,fluid)`
-
-#     Arguments:
-#     1. `πc`  : Pressure Ratio
-#     2. `h_in`: Inlet Enthalpy
-#     3. `p_in`: Inlet Pressure
-
-#     Output -> Outlet enthalpy after isentropic compression
-# """
-# function IsentropicCompression(πc, h_in, p_in,fluid)
-#     s = PropsSI("S", "H", h_in, "P", p_in, fluid)
-#     return PropsSI("H", "S", s, "P", πc * p_in, fluid)
-# end
-# @register_symbolic IsentropicCompression(πc, h_in, p_in,fluid)
-
 """
 `IsentropicCompression(πc, h_in, p_in,fluid,η)`
 
@@ -38,22 +22,6 @@ function IsentropicCompression(πc, h_in, p_in,fluid,η)
 end
 @register_symbolic IsentropicCompression(πc, h_in, p_in,fluid,η)
 export IsentropicCompression
-
-# """
-# `IsentropicExpansion(πc, h_in, p_in,fluid)`
-
-# * Arguments:
-#     1. `πc`  : Pressure Ratio
-#     2. `h_in`: Inlet Enthalpy
-#     3. `p_in`: Inlet Pressure
-
-# * Output -> Outlet enthalpy after isentropic expansion
-# """
-# function IsentropicExpansion(πc, h_in, p_in,fluid)
-#     s = PropsSI("S", "H", h_in, "P", p_in, fluid)
-#     return PropsSI("H", "S", s, "P", p_in/πc, fluid)
-# end
-# @register_symbolic IsentropicExpansion(πc, h_in, p_in,fluid)
 
 
 """
