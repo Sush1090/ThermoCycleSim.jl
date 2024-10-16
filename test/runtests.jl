9bbc05457aab373f58b6b023dbd6ca44ce268066
Sush1090 <sushrut1090@gmail.com>
2024-10-16 11:00:03 +0200

    added evap test

--- test/runtests.jl
+++ test/runtests.jl
@@ -39,4 +39,34 @@ bool2 = isapprox(sol[exp.s_in][1],sol[exp.s_out][1])
 @test isapprox(sol[source.h][1],sol[sink.h][1])
 end
 
+
+@testset "Evaporator phase" begin
+    out_phase = "gas"
+
+fluid = "R134A"
+@independent_variables t
+start_T = 300;
+start_p = PropsSI("P","Q",0,"T",start_T,fluid) + 1e3
+start_h = PropsSI("H","T",start_T,"P",start_p,fluid); start_mdot = 0.2 #kg/s
+
+@named source = MassSource(source_enthalpy = start_h,source_pressure = start_p,source_mdot = start_mdot,fluid = fluid)
+@named evporator = SimpleEvaporator(ΔT_sh = 5,fluid = fluid)
+@named sink = MassSink(fluid = fluid)
+
+eqs = [
+    connect(source.port,evporator.inport)
+    connect(evporator.outport,sink.port)
+]
+systems = [source,evporator,sink]
+@named test_evap = ODESystem(eqs, t, systems=systems)
+u0 = []
+tspan = (0.0, 1.0)
+sys = structural_simplify(test_evap)
+prob = ODEProblem(sys,u0,tspan,guesses = [])
+sol = solve(prob)
+
+phase_test = PhaseSI("T",sol[evporator.T_out][1],"P",sol[evporator.p_out][1],fluid)
+
+@test phase_test == out_phase
+end
 #@testset 
