e0e8fa8b1e19b5006f33ab7ad51f16d027bcf44b
Sush1090 <sushrut1090@gmail.com>
2024-10-16 17:42:13 +0200

    added Isothermal Compression

--- src/Processes.jl
+++ src/Processes.jl
@@ -46,5 +46,28 @@ end
 @register_symbolic IsentropicExpansion(πc, h_in, p_in,fluid,η)
 export IsentropicExpansion
 
+"""
+`IsothermalCompression(πc, h_in, p_in,fluid)`
 
+* Arguments:
+    1. `πc`   : Pressure Ratio
+    2. `h_in` : Inlet Enthalpy
+    3. `p_in` : Inlet Pressure
+    4. `fluid`: Fluid
 
+* Output -> Outlet enthalpy after Isothermal Compression
+"""
+function IsothermalCompression(πc, h_in, p_in,fluid)
+    T_in = PropsSI("T", "H", h_in, "P", p_in, fluid)
+    T_sat = PropsSI("T", "Q", 0, "P", p_in, fluid)
+    @assert abs(T_sat - T_in) > 1e-4
+    H_L = PropsSI("H", "Q", 0, "P", p_in, fluid)
+    H_G = PropsSI("H", "Q", 1, "P", p_in, fluid)
+    if H_L <= h_in <= H_G
+        throw(error("Cannot do Isothermal Compression in twophase region"))
+    end
+    h_out = PropsSI("H", "T", T_in, "P",πc*p_in, fluid)
+    return h_out
+end
+@register_symbolic IsothermalCompression(πc, h_in, p_in,fluid)
+export IsothermalCompression
