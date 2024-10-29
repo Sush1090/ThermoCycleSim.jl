64738457ae3967bf802421dd2fe4cc9d69dfabf1
Sush1090 <sushrut1090@gmail.com>
2024-10-29 11:48:57 +0100

    added Cycle plotting

--- src/Plotting/PhasePlot.jl
+++ src/Plotting/PhasePlot.jl
@@ -115,3 +115,79 @@ function PhasePlot(type::PhasePlotType_PH,sol::ODESolution,system::Vector{ODESys
 end
 
 export PhasePlot
+
+"""
+`CyclePlot(type::PhasePlotType_PH,sol::ODESolution,system::Vector{ODESystem},fluid::AbstractString=set_fluid)`
+
+Added Cycle ploting without saturation curve. Only plots the  cycle state points and their path on P-H plane. 
+
+ * Arguments:
+    1. `type`   : Chose between `PhasePlotType_PH` or `PhasePlotType_TS`
+    2. `sol`    : The solution from the ODE system. 
+    3. `system` : The vector of components chosen. The first should be `MassSource` and the last has to be `MassSink`.
+    4. `fluid`  : The fluid of chosen. Defaults to `set_fluid`
+"""
+function CyclePlot(type::PhasePlotType_PH,sol::ODESolution,system::Vector{ODESystem},fluid::AbstractString=set_fluid)
+    if isnothing(fluid)
+        throw(error("Fluid not selected"))
+    end
+    plot_sys = system[2:end-1];
+    propx = :p
+    propy = :h
+    pp = [sol[getproperty(i.inport, propx)][1] for i in plot_sys]
+    hh = [sol[getproperty(i.inport, propy)][1] for i in plot_sys]
+    p_data = []; h_data = []
+    cycle_data_pp = [pp;pp[1]]; cycle_data_hh = [hh;hh[1]]
+    for i = 1:size(pp,1)
+        x1 = cycle_data_pp[i]; y1 = cycle_data_hh[i];
+        x2 = cycle_data_pp[i+1]; y2 = cycle_data_hh[i+1];
+        h_ = collect(range(y1,y2,100)); h_data = append!(h_data,h_)
+        p_ = collect(range(x1,x2,100)); p_data = append!(p_data,p_)
+    end
+    fig1 =  scatter(hh,pp,label="Cycle Points")
+    plot!(h_data,p_data,label = "Cycle")
+    xlabel!("Enthalpy (J/kg)")
+    ylabel!("Pressure (Pa)")
+    return fig1
+end
+
+"""
+`CyclePlot(type::PhasePlotType_TS,sol::ODESolution,system::Vector{ODESystem},fluid::AbstractString=set_fluid)`
+
+Added Cycle ploting without saturation curve. Only plots the  cycle state points and their path on T-S plane. 
+
+ * Arguments:
+    1. `type`   : Chose between `PhasePlotType_PH` or `PhasePlotType_TS`
+    2. `sol`    : The solution from the ODE system. 
+    3. `system` : The vector of components chosen. The first should be `MassSource` and the last has to be `MassSink`.
+    4. `fluid`  : The fluid of chosen. Defaults to `set_fluid`
+"""
+function CyclePlot(type::PhasePlotType_TS,sol::ODESolution,system::Vector{ODESystem},fluid::AbstractString=set_fluid)
+    if isnothing(fluid)
+        throw(error("Fluid not selected"))
+    end
+    plot_sys = system[2:end-1];
+    propx = :p
+    propy = :h
+    pp = [sol[getproperty(i.inport, propx)][1] for i in plot_sys]
+    hh = [sol[getproperty(i.inport, propy)][1] for i in plot_sys]
+    T_cycle_points = PropsSI.("T","P",pp,"H",hh,fluid) 
+    s_cycle_points = PropsSI.("S","P",pp,"H",hh,fluid)
+    T_data = []; s_data = []
+    cycle_data_pp = [pp;pp[1]]; cycle_data_hh = [hh;hh[1]]
+    for i = 1:size(pp,1)
+        x1 = cycle_data_pp[i]; y1 = cycle_data_hh[i];
+        x2 = cycle_data_pp[i+1]; y2 = cycle_data_hh[i+1];
+        _,_,TT = LineData(T_line,x1,y1,x2,y2,fluid)
+        T_data = append!(T_data,TT)
+        _,_,ss = LineData(S_line,x1,y1,x2,y2,fluid)
+        s_data = append!(s_data,ss)
+    end
+    fig1 = plot(s_data,T_data,label = "Cycle")
+     scatter!(s_cycle_points,T_cycle_points,label="Cycle Points")
+     xlabel!("Entropy (J/kg/K)")
+     ylabel!("Temperature (K)")
+     return fig1
+end
+
+export CyclePlot
